2f2a351f69504c5e31d2f9d7e40db65190e30130
vikram <vikrambarandwal@gmail.com>
2021-10-22 18:41:23 +0530

    project files added. All features working well.

---
+++ main.js
@@ -0,0 +1,337 @@
+// Create our 'main' state that will contain the game
+var mainState = {
+    preload: function() { 
+        game.input.mouse.capture = true;
+
+        if(window.zilPay){
+        	console.log("Zilpay ready")
+        }else{
+        	console.log("Zilpay Not ready")
+        }
+
+        //generate a bird gaming spritesheet asset based on bird DNA and add into the game
+        url = buildBird(ContractState.token_mds[activeBird]);
+        game.load.spritesheet('bird', url, 70, 50, 40);
+
+		game.load.spritesheet('holePipe', 'assets/diamond.png', 32, 24, 5);
+		game.load.spritesheet('FlappyNFTTxt', 'assets/FlappyNFT.png', 200, 63, 2);
+		game.load.spritesheet('GameRestartBtn', 'assets/restartBtn.png', 180.5, 72, 2);
+		game.load.spritesheet('GameStartBtn', 'assets/startBtn.png', 154.5, 72, 2);
+		game.load.spritesheet('ConnectBtn', 'assets/connectBtn.png', 259.5, 72, 2);
+		game.load.spritesheet('ChangeBirdBtn', 'assets/changeBtnf.png', 200, 61, 2);
+        game.load.image('strtbtn', 'assets/strtbtn.png');
+        game.load.image('stpbtn', 'assets/stpbtn.png');
+        game.load.image('pipe0', 'assets/zillogo.png');
+        game.load.image('pipe1', 'assets/zilswaplogo.png');
+        game.load.image('pipe2', 'assets/scillalogo.png');
+        game.load.image('pipe3', 'assets/xcadlogo.png');
+        game.load.image('pipe4', 'assets/xsgdlogo.png');
+        game.load.image('pipe5', 'assets/packageportallogo.png');
+        game.load.audio('dies', 'assets/die.wav'); 
+        game.load.audio('wings', 'assets/wing.wav'); 
+        game.load.audio('points', 'assets/point.wav'); 
+        game.load.audio('btnclk', 'assets/btnclk.mp3'); 
+        game.load.image('backgroundsky','assets/fbg.png')
+
+
+        console.log("perload end")
+    },
+    loadBird: async function(){
+    	await loadNFTContract()
+    	console.log("Contract Loaded")
+		addr = ("0xb2684b4192ed878b3c7009d1bc5ae0c64b0508c0").toLowerCase()
+		this.ownedToken = []
+		console.log("list")
+		for(i in ContractState.token_owners)
+		    if(ContractState.token_owners[i] == addr) this.ownedToken.push(i)
+		token = ContractState.token_mds[this.ownedToken[0]]
+    	console.log(this.ownedToken.length + " NFT found")
+
+ 	    await fun(token);
+
+		aa = game.load.spritesheet('bird', url, 70, 50, 40);
+    },
+
+    create: function() { 
+        
+        this.background = game.add.tileSprite(0,0,800,490,'backgroundsky');
+
+        this.score = 0;
+        this.pipes = game.add.group(); 
+        this.holes = game.add.group(); 
+
+        // Set the physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.FlappyNFTTxt = game.add.sprite(game.world.centerX-100,16,'FlappyNFTTxt');
+		var FlappyNFTAnim = this.FlappyNFTTxt.animations.add('FlappyNFTAnim');
+		this.FlappyNFTTxt.animations.play('FlappyNFTAnim', 10, true);
+
+
+		this.bird = game.add.sprite(300, 110, 'bird');
+        this.bird.anchor.setTo(-0.2, 0.5); 
+        this.bird.scale.setTo(0.75,0.75);
+       	var fly = this.bird.animations.add('fly');
+
+
+        // Call the 'jump' function when the spacekey is hit
+        var spaceKey = game.input.keyboard.addKey(
+                        Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);  
+
+        //var tap = game.input.onTap.add(this.jump,this);
+
+        this.jumpSound = game.add.audio('wings');    
+        this.dieSound = game.add.audio('dies');    
+        this.pointSound = game.add.audio('points');    
+        this.btnclkSound = game.add.audio('btnclk');    
+
+        scoreText = game.add.text(game.world.centerX-24, 16, `${this.score}`, { fontSize: '48px', fill: '#000' }); 
+        scoreText.visible = false;
+
+        //startbutton = game.add.button(game.world.centerX - 95, 210, 'strtbtn', this.up, this, 2, 1, 0);
+		//var GameStartAnim = this.GameStartBtn.animations.add('GameStartAnim');
+        //startbutton.onInputDown.add(this.up,this);
+        //game.input.mouse.mouseDownCallback = "";
+		// button = game.add.button(game.world.centerX-96, game.world.centerY-35, 'button', this.actionOnClick, this, 2, 1, 0);
+		// button.onInputOver.add(this.over, this);
+	 //    button.onInputOut.add(this.out, this);
+	 //    button.onInputUp.add(this.up, this);
+
+       	if(window.zilPay.wallet.isConnect && window.zilPay.wallet.isEnable){
+			//zpc = game.add.text(140, 240, `ZilPay Connected`, { fontSize: '16px', fill: '#000' }); 
+        	this.GameStartBtn = game.add.button(game.world.centerX - 78, 210, 'GameStartBtn', this.up, this, 2, 1, 0);
+        	this.ChangeBirdBtn = game.add.button(game.world.centerX - 100, 290, 'ChangeBirdBtn', this.changeBird, this, 2, 1, 0);
+        }else{
+			this.zpnc = game.add.text(game.world.centerX - 90, 170, `ZilPay Not Connected`, { fontSize: '16px', fill: '#000' }); 
+        	this.ConnectBtn = game.add.button(game.world.centerX - 129.75, 210, 'ConnectBtn', this.connectWallet, this, 2, 1, 0);
+       	}
+    },
+    connectWallet: async function(){
+    	a = await window.zilPay.wallet.connect();
+    	if(a){
+        	this.GameStartBtn = game.add.button(game.world.centerX - 78, 210, 'GameStartBtn', this.up, this, 2, 1, 0);
+        	this.ChangeBirdBtn = game.add.button(game.world.centerX - 100, 290, 'ChangeBirdBtn', this.changeBird, this, 2, 1, 0);
+    		this.ConnectBtn.visible = false;
+    		this.zpnc.visible = false;
+    	}
+    },
+    update: function() {
+        // This function is called 60 times per second    
+        // It contains the game's logic
+ 
+        if (this.bird.y < 0 || this.bird.y > 490){
+            this.hitPipe();
+        }
+
+        if (this.bird.angle < 20)
+		    this.bird.angle += 1; 	
+
+		game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
+		game.physics.arcade.overlap(this.bird, this.holes, this.hitHole, null, this);
+		scoreText.setText(`${this.score}`);
+    },
+    // Make the bird jump 
+    jump: function() {
+    	if (this.bird.alive == false)
+    		return; 
+		
+		this.jumpSound.play();
+		this.bird.animations.play('fly', 160, false);
+
+        // Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -350;
+        var animation = game.add.tween(this.bird);
+		animation.to({angle: -20}, 100);
+		animation.start();
+    },
+
+    // Restart the game
+    restartGame: function() {
+		this.btnclkSound.play();
+        game.state.start('main');
+
+    },
+    changeBird: function(){
+    	ChangeBirdMenu();
+    },
+    addOnePipe: function(x, y,r) {
+        // Create a pipe at the position x and y
+        var pipe = game.add.sprite(x, y, 'pipe'+r);
+
+        // Add the pipe to our previously created group
+        this.pipes.add(pipe);
+
+        // Enable physics on the pipe 
+        game.physics.arcade.enable(pipe);
+
+        // Add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200; 
+
+        // Automatically kill the pipe when it's no longer visible 
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    addOneHole: function(x, y) {
+        // Create a pipe at the position x and y
+        var hole = game.add.sprite(x, y, 'holePipe');
+
+        // Add the pipe to our previously created group
+        this.holes.add(hole);
+
+        // Enable physics on the pipe 
+        game.physics.arcade.enable(hole);
+
+        // Add velocity to the pipe to make it move left
+        hole.body.velocity.x = -200; 
+
+        // Automatically kill the pipe when it's no longer visible 
+        hole.checkWorldBounds = true;
+        hole.outOfBoundsKill = true;
+		var point = hole.animations.add('point');
+	    hole.animations.play('point', 10, true);
+    }, 
+    addRowOfPipes: function() {
+        // Randomly pick a number between 1 and 5
+        // This will be the hole position
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        // Add the 6 pipes 
+        // With one big hole at position 'hole' and 'hole + 1'
+        r = Math.floor(Math.random() * 6);
+
+        for (var i = 0; i < 8; i++){
+            if (i != hole && i != hole + 1){
+                this.addOnePipe(800, i * 60 + 10,r);   
+            }else{
+            	this.addOneHole(800, i * 60 + 19);
+            }
+        }
+    },
+    hitPipe: function() {
+	    if (this.bird.alive == false)
+	        return;
+
+	    this.dieSound.play();
+	    this.bird.alive = false;
+//		stopbutton = game.add.button(game.world.centerX - 95, 210, 'stpbtn', this.restartGame, this, 2, 1, 0);
+        //stopbutton.input.mouse.mouseDownCallback = this.restarts; 
+
+   		this.GameRestartBtn = game.add.button(game.world.centerX - 91, 210, 'GameRestartBtn', this.restartGame, this, 2, 1, 0);
+		//var GameRestartAnim = this.GameRestartBtn.animations.add('GameOverAnim');
+
+	    game.time.events.remove(this.timer);
+
+	    this.pipes.forEach(function(p){
+	        p.body.velocity.x = 0;
+	    }, this);
+	    this.holes.forEach(function(p){
+	        p.body.velocity.x = 0;
+	    }, this);
+	},
+	hitHole: function(a,b) {
+	    if (this.bird.alive == false)
+	        return;
+	    this.pointSound.play();
+	    b.destroy();
+	    this.score += 1;
+	},
+	up: function(){
+		console.log("Up");
+		this.btnclkSound.play();
+		game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;  
+		this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);  
+		this.FlappyNFTTxt.destroy();
+        scoreText.visible = true;
+        this.GameStartBtn.visible = false;
+        this.ChangeBirdBtn.visible = false;
+
+	},
+	actionOnClick: function() {
+	    //background.visible =! background.visible;
+	    console.log("asdert")
+	},
+};
+
+var game;
+var color = ["SlateBlue","coral","fuchsia","gold","grey","lime","indigo","navy","OrangeRed","olive","SeaGreen","SkyBlue","crimson","teal","wheat","Sienna"];
+
+function loadGame(){
+
+	if(activeBird){
+		console.log("OnLoad called")
+		document.querySelector("#gallery-container").innerHTML = "";
+		// Initialize Phaser, and create a 400px by 490px game
+		game = new Phaser.Game(800, 490,Phaser.AUTO,"gallery-container");
+
+		// Add the 'mainState' and call it 'main'
+		game.state.add('main', mainState); 
+
+		// Start the state to actually start the game
+		game.state.start('main');
+	}else{
+		alert("You dont own any NFT, Pl own one to play");
+	}
+}
+
+function getG(x,y,r){
+    return `<g xmlns="http://www.w3.org/2000/svg" width="70" height="50" transform="translate(${x},${y})">
+            <circle style="stroke: rgb(0, 0, 0); fill: url(#body)" cx="41.7" cy="25" r="20"/>
+            <circle style="stroke: rgb(0, 0, 0); fill: white" cx="58.047" cy="12.53" r="5.949"/>
+            <circle xmlns="http://www.w3.org/2000/svg" style="stroke: rgb(0, 0, 0); fill: url(#eye);" cx="59" cy="12" r="2"/>
+            <g transform-origin="35 21.8" transform="rotate(${r})">
+                <rect x="5" y="10" width="34" height="8.5" style="fill: url(#wing); stroke: rgb(0, 0, 0);" rx="5" ry="5"/>
+                <rect x="10" y="17.5" width="29" height="8.5" style="fill: url(#wing); stroke: rgb(0, 0, 0);" rx="5" ry="5"/>
+                <rect x="15" y="25" width="24" height="8.5" style="fill: url(#wing); stroke: rgb(0, 0, 0);" rx="5" ry="5"/>    
+            </g>
+            <rect x="52.097" y="34.07" width="11.77" height="4.225" style="stroke: rgb(0, 0, 0); fill: rgb(247, 0, 0);" rx="10.566" ry="10.566"/>
+        </g>`;
+}
+
+function buildBird(token){
+	console.log("Build Bird", token)
+	b = parseInt("0x"+token[3]);
+	w = parseInt("0x"+token[5]);
+	e = parseInt("0x"+token[7]);
+
+    lst = []
+    x=20;
+    j=-2;
+    for(i=0;i<40;i++){
+        lst.push(x);
+        x = x+j;
+        if(x<=-20) j=2;
+    }
+
+	svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" id="svg" viewBox="0 0 700 200" width="700" height="200">
+		<defs xmlns="http://www.w3.org/2000/svg">
+			<linearGradient id="body">
+				<stop id="bodyC" stop-color="${color[b]}"/>
+			</linearGradient>
+			<linearGradient id="eye">
+				<stop id="eyeC" stop-color="${color[e]}"/>
+			</linearGradient>
+			<linearGradient id="wing">
+				<stop id="wingC" stop-color="${color[w]}"/>
+			</linearGradient>
+		</defs>
+    `;
+
+    for(y=0;y<4;y++){
+        for(x=0;x<10;x++){
+            svg += getG(x*70,y*50,lst[y*10+x]);
+        }
+    }
+
+    svg += "</svg>";
+
+    blob = new Blob([svg],{type: 'image/svg+xml'});
+    
+    console.log("blob")
+    url = URL.createObjectURL(this.blob);
+    console.log("url")
+    
+    return url;
+}
---
+++ script.js
@@ -0,0 +1,342 @@
+var ContractAddress;
+var ContractObject;
+var ContractState;
+var ownedNFT;
+var activeBird;
+
+var color = ["SlateBlue","coral","fuchsia","gold","grey","lime","indigo","navy","OrangeRed","olive","SeaGreen","SkyBlue","crimson","teal","wheat","Sienna"];
+
+function checkWallet(){
+	if(window.zilPay){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+async function connectWallet(){
+	return (await window.zilPay.wallet.connect());
+}
+
+function loadContract(contractAddr){
+	try{
+		return window.zilPay.contracts.at(contractAddr);
+	}catch(err){
+		console.log(err.message);
+		return false;
+	}
+}
+
+function onloadInit(){
+	connectAppToWallet();
+	observer();
+	console.clear();
+	loadNFTContract();
+}
+
+function loadGallery(flag){
+	document.querySelector("#loadingBox").style.setProperty("visibility","visible");
+	document.querySelector("#loadingBox").innerHTML = "Loading Gallery....";
+	if(!ContractObject){
+		alert("Contract not loaded, please wait");
+		document.querySelector("#loadingBox").style.setProperty("visibility","hidden");
+		loadNFTContract();
+		return;
+	}
+
+	var gallery = document.querySelector("#gallery-container");
+	gallery.innerHTML = "";
+
+	tokenOwners = ContractState.token_owners;
+	tokenMds = ContractState.token_mds;
+
+	galleryCode = "<h2 style='width:100%' class='HVCenter'>" + ((flag)?"Your ":"All Minted ") + ContractObject.init[1].value + "</h2>";
+
+	var currentAccountAddress = window.zilPay.wallet.defaultAccount.base16;
+
+	ownedNFT = [];
+	for(i in tokenOwners){
+
+		if(tokenOwners[i].toUpperCase() == currentAccountAddress.toUpperCase()){			
+			ownedNFT.push(i);
+		}else{
+			if(flag){
+				continue;
+			}
+		}
+
+		var transferBtn = "";
+		if(tokenOwners[i].toUpperCase() == currentAccountAddress.toUpperCase()){			
+			transferBtn = "<button onclick='transferNFT(" + i + ")'>Transfer</button>"
+		}		
+
+		md1 = parseInt("0x"+tokenMds[i][3]);
+		md2 = parseInt("0x"+tokenMds[i][5]);
+		md3 = parseInt("0x"+tokenMds[i][7]);
+		galleryCode += `
+		<div id="nft-${i}" class="nft-card">
+			<div class="nft-card-id HVCenter">NFT ID: ${i}&nbsp;${transferBtn}</div>
+			<div class="nft-card-img-holder HVCenter">
+				${buildBrd(md1,md2,md3,i)}
+			</div>
+			<div class="nft-card-owner">Owner:&nbsp;${tokenOwners[i]}</div>
+		</div>
+		`;
+	}
+
+	if(ContractState.total_supply <= 0){
+		if(!flag)
+			galleryCode += "<h3>No NFTs Minted So Far..</h3>";
+	}
+
+	if(ownedNFT.length > 0){
+		activeBird = ownedNFT[0];
+	}else{
+		if(flag){
+			galleryCode += "<h3>You Dont Own Any NFT</h3>"
+		}
+		activeBird = undefined;
+	}
+
+	gallery.innerHTML = galleryCode;
+	
+	document.querySelector("#loadingBox").style.setProperty("visibility","hidden");
+}
+
+function loadMarket(){
+	document.querySelector("#loadingBox").style.setProperty("visibility","visible");
+	console.log(document.querySelector("#loadingBox").style.visibility);
+	document.querySelector("#loadingBox").innerHTML = "<h1>Loading Market....</h1>";
+
+	if(!ContractObject){
+		alert("Contract not loaded, please wait");
+		document.querySelector("#loadingBox").style.setProperty("visibility","hidden");
+		loadNFTContract()
+		return;
+	}
+
+
+	var gallery = document.querySelector("#gallery-container");
+	gallery.innerHTML = "Please wait while market is loaded";
+
+	mdToId = ContractState.metadata_to_id;
+	
+	galleryCode = "<h2 style='width:100%' class='HVCenter'>flappyNFT Market</h2>";
+
+	for (i = 0;i<=0xf; i++) {
+		for (j = 0;j<=0xf; j++) {
+			if(i == j) continue;
+			for (k = 0;k<=0xf; k++) {
+				if(k == j || i==k) continue;
+					
+					x=i.toString(16);
+					y=j.toString(16);
+					z=k.toString(16);
+
+					var dna = `0x0${z}0${y}0${x}`;
+
+					if(!mdToId[dna]){
+						galleryCode += `
+						<div class="nft-card">
+							<div class="nft-card-id HVCenter">NFT DNA:&nbsp;${dna}</div>
+							<div class="nft-card-img-holder HVCenter">
+								${buildBrd(k,j,i,dna)}
+							</div>
+							<div class="nft-card-owner"><button onclick="buyNFT('${z}','${y}','${x}')"">Buy</button></div>
+						</div>
+						`;
+					}
+			}			
+		}		
+	}
+
+	gallery.innerHTML = galleryCode;
+	document.querySelector("#loadingBox").style.setProperty("visibility","hidden");
+}
+
+
+async function connectAppToWallet(){
+	check1 = checkWallet();
+	check2 = await connectWallet();
+	if(check1 && check2){
+		//if successful hide button and show net and address
+		document.querySelector("#wallet-address-container").style.display = "inline-block";
+		document.querySelector("#connect-button-container").style.display = "none";
+
+		//get and set network and address
+		let networkName = window.zilPay.wallet.net;
+		let currentAddress = window.zilPay.wallet.defaultAccount.bech32;
+		document.querySelector("#wallet-network-span").innerHTML = networkName;
+		document.querySelector("#wallet-address-span").innerHTML = currentAddress;
+	}else{
+		//if connection failed 
+		alert("Something went wrong connecting wallet, try again later.");
+	}
+}
+
+function observer(){
+	window.zilPay.wallet.observableAccount().subscribe(function (acc){
+		//alert("Account Changed. Please Reload App..");
+		if(acc) connectAppToWallet();
+	});
+
+	window.zilPay.wallet.observableNetwork().subscribe(function (net){
+		if(net) connectAppToWallet();
+		//alert("Network Changed. Please Reload App..");
+	});
+}
+
+
+function loadNFTContract(){
+	document.querySelector("#loadingBox").style.setProperty("visibility","visible")
+	document.querySelector("#loadingBox").innerHTML = "<h1>Loading Contract....</h1>";
+
+	var contractAddress = "zil133z8p0thl9chljaf5l42chpygceknyx6ycgl8a";
+	
+	ContractObject = loadContract(contractAddress);
+	if(ContractObject){
+		ContractObject.getState().then(function(stateData){
+			ContractState = stateData;
+			ContractAddress = contractAddress;
+			// alert("Contract State Loaded Successfully!");
+			document.querySelector("#gallery-container").innerHTML = "Building Gallery";
+			ContractObject.getInit().then(function(x){
+				document.querySelector("#loadingBox").style.setProperty("visibility","hidden");
+				loadGallery(false);
+			});
+
+		});
+	}else{
+		ContractObject = undefined;
+	}
+}
+
+function buyNFT(x,y,z){
+
+	if(!ContractObject){
+		alert("Please load contract first");
+		return;
+	}
+
+	md1 = `0x0${x}`;
+	md2 = `0x0${y}`;
+	md3 = `0x0${z}`;
+
+	/* Code for transaction call */
+	const gasPrice = window.zilPay.utils.units.toQa('2000', window.zilPay.utils.units.Units.Li);
+
+	var tx = ContractObject.call('Mint',[{
+		vname: "md1",
+		type: "ByStr1",
+		value: md1
+	},{
+		vname: "md2",
+		type: "ByStr1",
+		value: md2
+	},{
+		vname: "md3",
+		type: "ByStr1",
+		value: md3
+	}],
+	{
+		amount: 25000000000000,
+		gasPrice: gasPrice,
+		gasLimit: window.zilPay.utils.Long.fromNumber(10000)
+	});
+	/* Code for transaction call */
+
+	//handle the promise accordingly
+	tx.then(function(a){
+		alert(`Transaction ID: ${a.ID}`);
+		alert(`Please refresh when Minted.`)
+	});
+}
+
+
+function transferNFT(nftid){
+
+	var receiverAddress = prompt("Please enter the address you want to send NFT ID:" + nftid + " to");
+
+	/* Code for transaction call */
+	const gasPrice = window.zilPay.utils.units.toQa('2000', window.zilPay.utils.units.Units.Li);
+
+	var tx = ContractObject.call('Transfer',[{
+		vname: "to",
+		type: "ByStr20",
+		value: receiverAddress+""
+	},{
+		vname: "token_id",
+		type: "Uint32",
+		value: nftid+""
+	}],
+	{
+		gasPrice: gasPrice,
+		gasLimit: window.zilPay.utils.Long.fromNumber(10000)
+	});
+	/* Code for transaction call */
+
+	//handle the promise accordingly
+	tx.then(function(a){
+		console.log(a);
+	});
+}
+
+function buildBrd(md1,md2,md3,i){
+
+	var brd = `
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 50" width="140" height="100">	
+	<g xmlns="http://www.w3.org/2000/svg" width="70" height="50" transform="translate(0,0)">
+		<circle style="stroke: rgb(0, 0, 0); fill: ${color[md1]}" cx="41.7" cy="25" r="20"/>
+		<circle style="stroke: rgb(0, 0, 0); fill: white;" cx="58.047" cy="12.53" r="5.949"/>
+		<circle style="fill: ${color[md3]}" cx="58.047" cy="12.53" r="3"/>
+		<g transform-origin="35 21.8" transform="rotate(20)">
+			<rect x="5" y="10" width="34" height="8.5" style="fill: ${color[md2]}; stroke: rgb(0, 0, 0);" rx="5" ry="5"/>
+			<rect x="10" y="17.5" width="29" height="8.5" style="fill: ${color[md2]}; stroke: rgb(0, 0, 0);" rx="5" ry="5"/>
+			<rect x="15" y="25" width="24" height="8.5" style="fill: ${color[md2]}; stroke: rgb(0, 0, 0);" rx="5" ry="5"/>    
+		</g>
+		<rect x="52.097" y="34.07" width="11.77" height="4.225" style="stroke: rgb(0, 0, 0); fill: red;" rx="10.566" ry="10.566"/>
+	</g>
+	</svg>
+	`;
+	return brd;
+}
+
+document.querySelector("#loadingBox").style.visibility = "hidden";
+
+function ChangeBirdMenu(){
+	document.querySelector("#loadingBox").style.visibility = "visible";
+	
+	var gallery = document.querySelector("#loadingBox");
+	gallery.innerHTML = "";
+
+	tokenOwners = ContractState.token_owners;
+	tokenMds = ContractState.token_mds;
+
+	galleryCode = "<div id='selectBirdDiv'><h2 style='width:100%' class='HVCenter'>Select Bird</h2>";
+
+	for(i in ownedNFT){
+		setBtn = "<button onclick='setActiveBird(" + ownedNFT[i] + ")'>Set</button>";
+
+		md1 = parseInt("0x"+tokenMds[ownedNFT[i]][3]);
+		md2 = parseInt("0x"+tokenMds[ownedNFT[i]][5]);
+		md3 = parseInt("0x"+tokenMds[ownedNFT[i]][7]);
+		galleryCode += `
+		<div id="nft-${i}" class="nft-card">
+			<div class="nft-card-id HVCenter">NFT ID: ${i}&nbsp;${setBtn}</div>
+			<div class="nft-card-img-holder HVCenter">
+				${buildBrd(md1,md2,md3,i)}
+			</div>
+			<div class="nft-card-owner">Owner:&nbsp;${tokenOwners[i]}</div>
+		</div>
+		`;
+	}
+	galleryCode += "</div>";
+	gallery.innerHTML = galleryCode;
+
+}
+
+function setActiveBird(id){
+	activeBird = id;
+	mainState.restartGame();
+	document.querySelector("#loadingBox").style.visibility = "hidden";
+}
